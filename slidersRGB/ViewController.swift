d9ca6d51ba62c596504fcf6538ba16909ffe597a
Mikhail M <milyusha-mv@ural.rt.ru>
2020-08-22 20:36:05 +0500

    Initial Commit

---
+++ slidersRGB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slidersRGB
+//
+//  Created by Михаил Милюша on 22.08.2020.
+//  Copyright © 2020 Mikhail Miliusha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
