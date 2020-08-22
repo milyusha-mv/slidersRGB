9345e17e7ee6b51d3be14231adf54d7ba260ce17
Mikhail M <milyusha-mv@ural.rt.ru>
2020-08-22 23:17:17 +0500

    do sliders

--- slidersRGB/ViewController.swift
+++ slidersRGB/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var valueRedLabel: UILabel!
+    @IBOutlet var valueGreenLabel: UILabel!
+    @IBOutlet var valueBlueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var mainLabel: UIView!
+    
+    private let minValue: Float = 0
+    private let maxValue: Float = 255
+    
+    private let step: Float = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSlider.minimumValue = minValue
+        redSlider.maximumValue = maxValue
+        
+        greenSlider.minimumValue = minValue
+        greenSlider.maximumValue = maxValue
+        
+        blueSlider.minimumValue = minValue
+        blueSlider.maximumValue = maxValue
+        
     }
 
-
+    @IBAction func changeValueRedSlider() {
+        let roundedValue = round(redSlider.value / step) * step
+        redSlider.value = roundedValue
+        valueRedLabel.text = String(redSlider.value)
+        
+    }
+    
+    @IBAction func changeValueGreenSlider() {
+        let roundedValue = round(greenSlider.value / step) * step
+        greenSlider.value = roundedValue
+        valueGreenLabel.text = String(greenSlider.value)
+    }
+    
+    @IBAction func changeValueBlueSlider() {
+        let roundedValue = round(blueSlider.value / step) * step
+        blueSlider.value = roundedValue
+        valueBlueLabel.text = String(blueSlider.value)
+    }
 }
 
