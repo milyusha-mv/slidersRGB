e080fa3072439d6eab7f16b958f9208fca9d3989
Mikhail M <milyusha-mv@ural.rt.ru>
2020-08-23 00:24:21 +0500

    fix

--- slidersRGB/ViewController.swift
+++ slidersRGB/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    @IBOutlet var mainLabel: UIView!
+    @IBOutlet var mainView: UIView!
     
     private let minValue: Float = 0
     private let maxValue: Float = 255
@@ -36,25 +36,51 @@ class ViewController: UIViewController {
         blueSlider.minimumValue = minValue
         blueSlider.maximumValue = maxValue
         
+        valueRedLabel.text = String(redSlider.value)
+        valueGreenLabel.text = String(greenSlider.value)
+        valueBlueLabel.text = String(blueSlider.value)
+        
     }
 
     @IBAction func changeValueRedSlider() {
         let roundedValue = round(redSlider.value / step) * step
         redSlider.value = roundedValue
+        
         valueRedLabel.text = String(redSlider.value)
         
+        mainView.backgroundColor = .init(
+            red: CGFloat(redSlider.value/255),
+            green: CGFloat(greenSlider.value/255),
+            blue: CGFloat(blueSlider.value/255),
+            alpha: 1.0)
     }
     
     @IBAction func changeValueGreenSlider() {
         let roundedValue = round(greenSlider.value / step) * step
         greenSlider.value = roundedValue
+        
         valueGreenLabel.text = String(greenSlider.value)
+        
+        mainView.backgroundColor = .init(
+            red: CGFloat(redSlider.value/255),
+            green: CGFloat(greenSlider.value/255),
+            blue: CGFloat(blueSlider.value/255),
+            alpha: 1.0)
     }
     
     @IBAction func changeValueBlueSlider() {
         let roundedValue = round(blueSlider.value / step) * step
         blueSlider.value = roundedValue
+        
         valueBlueLabel.text = String(blueSlider.value)
+        
+        mainView.backgroundColor = .init(
+            red: CGFloat(redSlider.value/255),
+            green: CGFloat(greenSlider.value/255),
+            blue: CGFloat(blueSlider.value/255),
+            alpha: 1.0)
     }
+    
+    
 }
 
