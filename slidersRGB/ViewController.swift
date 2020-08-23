593a460db791b39f255d4a9e5a839dc4e1d9b0ae
Mikhail M <milyusha-mv@ural.rt.ru>
2020-08-23 23:16:24 +0500

    work after teacher's comments

--- slidersRGB/ViewController.swift
+++ slidersRGB/ViewController.swift
@@ -20,49 +20,27 @@ class ViewController: UIViewController {
     
     @IBOutlet var mainView: UIView!
     
-    private let minValue: Float = 0
-    private let maxValue: Float = 255
-    
     private let stepSlider: Float = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redSlider.minimumValue = minValue
-        redSlider.maximumValue = maxValue
-        
-        greenSlider.minimumValue = minValue
-        greenSlider.maximumValue = maxValue
-        
-        blueSlider.minimumValue = minValue
-        blueSlider.maximumValue = maxValue
-        
         valueRedLabel.text = String(format: "%.0f", redSlider.value)
         valueGreenLabel.text = String(format: "%.0f", greenSlider.value)
         valueBlueLabel.text = String(format: "%.0f", blueSlider.value)
         
         setMainViewColor()
-        
     }
 
-    @IBAction func changeValueRedSlider() {
-        let roundedValue = round(redSlider.value / stepSlider) * stepSlider
-        redSlider.value = roundedValue
+    @IBAction func changeValueSlider() {
+        redSlider.value = round(redSlider.value / stepSlider) * stepSlider
+        greenSlider.value = round(greenSlider.value / stepSlider) * stepSlider
+        blueSlider.value = round(blueSlider.value / stepSlider) * stepSlider
+        
         valueRedLabel.text = String(format: "%.0f", redSlider.value)
-        setMainViewColor()
-    }
-    
-    @IBAction func changeValueGreenSlider() {
-        let roundedValue = round(greenSlider.value / stepSlider) * stepSlider
-        greenSlider.value = roundedValue
         valueGreenLabel.text = String(format: "%.0f", greenSlider.value)
-        setMainViewColor()
-    }
-    
-    @IBAction func changeValueBlueSlider() {
-        let roundedValue = round(blueSlider.value / stepSlider) * stepSlider
-        blueSlider.value = roundedValue
         valueBlueLabel.text = String(format: "%.0f", blueSlider.value)
+        
         setMainViewColor()
     }
     
@@ -73,6 +51,5 @@ class ViewController: UIViewController {
             blue: CGFloat(blueSlider.value/255),
             alpha: 1.0)
     }
-    
 }
 
